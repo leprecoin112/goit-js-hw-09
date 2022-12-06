6266c8d35513b201e816fdd3f04f073f9289b7b6
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-12-06 19:55:58 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = 0;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(setBodyColor, 1000);
+  disableStartBtn(true);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  disableStartBtn(false);
+}
+
+function disableStartBtn(isDisable) {
+  refs.startBtn.disabled = isDisable;
+}
+
+function setBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
