c85c3cc1e1e0305031960ca59d6dab37e0100abb
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-12-06 22:18:23 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const amount = getNumber(e.target.amount.value);
+  const step = getNumber(e.target.step.value);
+  const delay = getNumber(e.target.delay.value);
+
+  generatePromise({ delay, step, amount });
+}
+
+function getNumber(value) {
+  return Number.parseInt(value);
+}
+
+function generatePromise({ delay, step, amount }) {
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
